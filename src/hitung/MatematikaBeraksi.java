9825b1f33d26b4288d9900007207a0e98377b503
MuhammadZainiAbdurrahman <ezennn17@gmail.com>
2023-06-12 19:12:38 +0800

    Membuat Class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,14 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        Matematika zaini = new Matematika(3,2);
+        
+        System.out.println("Hasil Penjumlahan : "+zaini.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : "+zaini.setPengurangan());
+        System.out.println("Hasil Pengalian : "+zaini.setPerkalian());
+        System.out.println("Hasil Penbagian : "+zaini.setPembagian());
+        
+       
+    }
 }
